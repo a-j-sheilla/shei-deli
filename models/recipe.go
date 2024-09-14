956ba433d4aff7d09b3c3dc31e5eff2e6fc9de1a
a-j-sheilla <sheillajuma001@gmail.com>
2024-09-14 12:12:00 +0300

    updated the recipe model comments

--- models/recipe.go
+++ models/recipe.go
@@ -2,12 +2,12 @@ package models
 
 import "gorm.io/gorm"
 
-// Recipe model to store recipe data
+// this mdel stores recipe data
 type Recipe struct {
     gorm.Model
     Title        string
     Ingredients  string
     Instructions string
     Category     string
-    APIRecipeID  int `gorm:"default:null"` // This can store recipe ID from Spoonacular API
+    APIRecipeID  int `gorm:"default:null"` // stores the recipe ID from Spoonacular API
 }
